4c1d2b0679f9b58f4ff73038c036843f1e6316db
Nikola Banko <nikola.banko@infobip.com>
2013-02-26 11:41:23 +0100

    added DeliveryStatus.cs enum

---
+++ OneApi/Model/DeliveryStatus.cs
@@ -0,0 +1,19 @@
+﻿namespace org.infobip.oneapi.model
+{
+    /// <summary>
+    /// Use StringEnum.GetStringValue(DeliveryStatus.[VALUE]) to get the string value of the Enum
+    /// </summary>
+    public enum DeliveryStatus
+    {
+        [StringValue("DeliveredToTerminal")]
+        DELIVERED_TO_TERMINAL,
+        [StringValue("DeliveryUncertain")]
+        DELIVERY_UNCERTAIN,
+        [StringValue("DeliveryImpossible")]
+        DELIVERY_IMPOSSIBLE,
+        [StringValue("MessageWaiting")]
+        MESSAGE_WAITING,
+        [StringValue("DeliveredToNetwork")]
+        DELIVERED_TO_NETWORK
+    }
+}
